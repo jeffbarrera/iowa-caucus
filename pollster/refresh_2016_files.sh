4d49241831800fdfc844ab8a9c378c6180675410
Jacob Fenton <jfenton@sunlightfoundation.com>
2016-01-27 14:23:40 -0800

    and file retriever

---
+++ pollster/refresh_2016_files.sh
@@ -0,0 +1,3 @@
+curl "http://elections.huffingtonpost.com/pollster/2016-iowa-presidential-republican-caucus.csv" > data/2016-iowa-presidential-republican-caucus.csv
+curl "http://elections.huffingtonpost.com/pollster/2016-national-gop-primary.csv" > data/2016-national-gop-primary.csv
+
