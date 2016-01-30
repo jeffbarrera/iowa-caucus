3b40359d6a1de5e3e1528c8c719d363e8eb98daf
Jacob Fenton <jfenton@sunlightfoundation.com>
2016-01-29 22:12:55 -0800

    test ksmooth (gaussian) and lowess regressions. epanechnikov still wins

---
+++ jf_utils.py
@@ -0,0 +1,84 @@
+import csv
+
+from rpy2 import robjects
+from rpy2.robjects.packages import importr
+
+# for lpepa:
+importr('lpridge')
+
+candidates = {
+    2008:['GIULIANI','HUCKABEE','MCCAIN','PAUL','ROMNEY','THOMPSON'],
+    2012:['Romney','Gingrich','Santorum','Paul','Cain','Huntsman','Perry'],
+    2016:['Trump','Cruz','Rubio','Carson','Bush','Christie','Rand Paul','Kasich','Huckabee','Fiorina','Santorum','Gilmore','Graham','Jindal','Pataki','Perry','Walker']
+}
+
+# note capitalization. Sigh. 
+results = {
+            2008: 
+            {
+                'HUCKABEE':34.4,
+                'ROMNEY':25.2,
+                'THOMPSON':13.4,
+                'MCCAIN':13,
+                'PAUL':9.9,
+                'GIULIANI':3.4
+            },
+            2012:
+            {
+                'Santorum':24.6,
+                'Romney':24.5,
+                'Paul':21.4,
+                'Gingrich':13.3,
+                'Perry':10.3,
+                'Bachmann':5.0,
+                'Huntsman':0.6,
+                'Cain':0.0
+            }
+        }
+        
+def get_file_as_named_rows(filename):
+    reader = csv.reader(open(filename, 'r'))
+    results = {}
+    header = reader.next()
+    for colname in header:
+        results[colname] = []
+    
+    for row in reader:
+        for i, value in enumerate(row):
+            results[header[i]].append(value)
+    return results
+
+def convert_nonints_to_na(array):
+    count=0
+    for i, val in enumerate(array):
+        try:
+            int(val)
+        except ValueError:
+            array[i]='NA'
+            count+=1
+            
+    return [array, count]
+    
+    
+# estimators
+
+def estimate_epa(bandwidth):
+    
+    robjects.r("cand_epan <- lpepa(df$dtc, df$huck, bandw=%s, n.out=100)" % bandwidth)
+    est_value = robjects.r('''cand_epan$est[100]''')[0]
+    
+    return est_value
+
+def estimate_ksmooth(bandwidth):
+
+    robjects.r("gaussian_smooth <- ksmooth(df$dtc, df$huck, \"normal\", bandwidth=%s, n.points=100)" % bandwidth)
+    est_value = robjects.r('''gaussian_smooth$y[100]''')[0]
+
+    return est_value
+
+def estimate_lowess(f):
+
+    robjects.r("cand_lowess <- lowess(df$dtc, df$huck, f=%s)" % f)
+    est_value = robjects.r('''cand_lowess$y[length(cand_lowess$y)]''')[0]
+
+    return est_value
---
+++ test_lowess.py
@@ -0,0 +1,90 @@
+import csv
+
+from jf_utils import get_file_as_named_rows, candidates, results, convert_nonints_to_na, estimate_epa, estimate_ksmooth, estimate_lowess
+
+# requires rpy2 ; install w/ 'pip install rpy2'
+from rpy2 import robjects
+
+
+
+
+if __name__ == "__main__":
+    
+    field_names = ['window_type', 'bw_value', 'year', 'scope', 'candidate', 'result', 'estimate', 'squared_error']
+    outfilename  = "lowesslog.csv"
+    outfile = open(outfilename, 'w')
+    outfile.write(",".join(field_names) +"\n")
+    dw = csv.DictWriter(outfile, fieldnames=field_names, restval='', extrasaction='ignore')
+    
+    mse_field_names = ['window_type', 'bw_value', 'scope', 'mse', 'se']
+    mse_outfilename  = "lowesslog_mse.csv"
+    mse_outfile = open(mse_outfilename, 'w')
+    mse_outfile.write(",".join(mse_field_names) +"\n")
+    mse_dw = csv.DictWriter(mse_outfile, fieldnames=mse_field_names, restval='', extrasaction='ignore')
+    
+    # super inefficent to have this in outer loop, but conceptually easier to get cyclewise-error
+    for bandwidth in [0.5,0.1,0.15,0.2,0.25,0.3,0.35,0.4]:
+        print ">>> BANDWIDTH: %s" % bandwidth
+        
+        for scope in ['iowa']:
+
+            for year in [2008, 2012]:
+
+                print "Handling %s %s" % (year, scope)
+                result = get_file_as_named_rows('pollster/data/cleaned_' + str(year) + '_' + scope + '.csv')
+                days_to_caucus = result['days_to_caucus']
+                dtc_count = len(days_to_caucus)
+            
+                # r object for dtc
+                dtc_vec = robjects.IntVector(days_to_caucus)
+                
+                error_array = []
+            
+                #print "dtc:\n%s" % days_to_caucus
+                for candidate in candidates[year]:
+                
+                    candidate_result = results[year][candidate]
+                
+                    candidate_data = result[candidate]
+                    cd_count = len(candidate_data)
+                    # we got problems if these rows are different length
+                    assert dtc_count==cd_count
+                    [candidate_data, count] = convert_nonints_to_na(candidate_data)
+                    if len(candidate_data)==count:
+                        # no regression to run if it's all NAs
+                        continue
+                
+                    candidate_vector_raw = "c(" + ",".join(candidate_data) + ")"
+                    #print candidate_vector_raw
+                    robjects.r("this_cand<-%s" % candidate_vector_raw)
+                    robjects.r("df = data.frame(%s, this_cand)" % (dtc_vec.r_repr()))
+                    
+                    
+                    robjects.r('''
+                        colnames(df) <- c('dtc', 'huck')
+                        df<-na.omit(df)
+                        df$dtc = -df$dtc
+                        df <- subset(df, dtc<1)
+                    ''')
+                    
+                    
+                    # apparently the robjects namespace works from there
+                    #est_value = estimate_epa(bandwidth)
+                    est_value = estimate_lowess(bandwidth)
+                    squared_error = (candidate_result - est_value)*(candidate_result - est_value)
+                    row_result = {'window_type':'lowess', 'bw_value':bandwidth, 'year':year, 'scope':scope, 'candidate':candidate, 'result':candidate_result, 'estimate':est_value, 'squared_error':squared_error}
+                    print row_result
+                    
+                    error_array.append(squared_error)
+                    
+                    dw.writerow(row_result)
+                    
+                
+            se = sum(error_array)
+            count = len(error_array)
+            mse = se/count
+            mse_result = {'window_type':'lowess', 'bw_value':bandwidth, 'scope':scope, 'mse':mse, 'se':se}
+            mse_dw.writerow(mse_result)
+        
+        
+                    
